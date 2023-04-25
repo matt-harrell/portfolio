dcee27ef709336581a10a28a9c76e5c78ded1626
Matt Harrell <matthew.629@hotmail.com>
2023-04-24 22:00:14 -0400

    add way from blue bar to change on different pages

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -5,23 +5,24 @@ import { globalHistory, useLocation } from '@reach/router'
 
 function NavBar() {
 
-    const [route,setRoute] = useState<string>('');
+    const [route, setRoute] = useState<string>('');
     const location = useLocation();
 
-useEffect(() => {
-    if (window.location.href.includes('#')){
-        
-        setRoute(window.location.href.split('#')[1])
-    }else{
-        setRoute(window.location.href.split('/')[1])
-    }
-}, [location])
+    useEffect(() => {
+        if (window.location.href.includes('#')) {
+            const url = window.location.href.split('#');
+            setRoute(url[url.length - 1])
+        } else {
+            const url = window.location.href.split('/');
+            setRoute(url[url.length - 1])
+        }
+    }, [location])
 
     const navItems = [
         ['About', '/'],
-        ['Projects', '#projects'],
+        ['Projects', '/#projects'],
         ['Resume', '/resume'],
-        ['Contact', '#contact'],
+        ['Contact', '/#contact'],
     ]
 
     return (
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,39 @@
 import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
 import Layout from "../components/Layout"
+import { useEffect } from "react"
 
 
 
 const IndexPage: React.FC<PageProps> = () => {
+
+  /*
+    todo: create useref for cotact an project section
+    if either is above the middle of screen change href location
+    that should update the route and trigger the blue bar to change
+  */
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    }
+  }, [])
+
+  const handleScroll = () => {
+    console.log('scroll');
+  }
+
   return (
     <Layout>
-      <main>
       <h1 className="text-3xl font-bold underline h-screen">
         Hello world!
       </h1>
       <div id="contact" className="h-screen pt-14">
         contact
       </div>
-    </main>
     </Layout>
-    
+
   )
 }
 
