fcd6eeb8138d5d232cd65e5528e3b34d5f360a42
Matt Harrell <matthew.629@hotmail.com>
2023-05-19 13:31:18 -0400

    add a title

--- src/components/ProjectSection/ProjectSectionComp.tsx
+++ src/components/ProjectSection/ProjectSectionComp.tsx
@@ -14,11 +14,17 @@ interface ProjectSectionCompProps{
 function ProjectSectionComp({data,projectSectionRef}:ProjectSectionCompProps) {
 
   return (
-    <section className='grid xl:grid-cols-4 md:grid-cols-2 grid-cols-1 lg:gap-10 gap-8 container mx-auto lg:px-6 md:px-4 px-3' ref={projectSectionRef}>
-      {data.allMdx.nodes.map((node) => 
-        <ProjectTile node={node} key={node.id}/>
-      )}
+    <section>
+      <h2 className='text-center font-bold text-6xl mb-16 text-white'>
+        Projects
+      </h2>
+      <div className='px-3 grid md:grid-cols-2 grid-cols-1 lg:gap-10 gap-8 container mx-auto lg:px-6 md:px-4 ' ref={projectSectionRef}>
+        {data.allMdx.nodes.map((node) =>
+          <ProjectTile node={node} key={node.id} />
+        )}
+      </div>
     </section>
+    
   )
 }
 
